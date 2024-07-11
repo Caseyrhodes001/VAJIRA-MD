841b73db12f6b550d72fa05bc7626289b276634f
Caseyrhodes001 <167879273+Caseyrhodes001@users.noreply.github.com>
2024-07-11 22:25:17 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,8 @@ function convertToBool(text, fault = 'true') {
 global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
 const BOTNAME = 'VAJIRA-MD|-WA-BOT™';
 const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
-global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
-    
+global.owner = process.env.OWNER_NUMBER  || '254112192119' ;  // ADD YOUR NUMBER WITHOUT +
+
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || 'youre session id' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
